9fb1ae5ce4944efb976b3c74216ec2715f59ee96
Ted M. Young <tedyoung@gmail.com>
2022-02-21 11:55:17 -0800

    LAB #2: Moved cardsAsString to ConsoleHand

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -2,9 +2,6 @@ package com.jitterted.ebp.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class Hand {
     private final List<Card> cards = new ArrayList<>();
@@ -40,14 +37,7 @@ public class Hand {
     }
 
     void display() {
-        System.out.println(cardsAsString());
-    }
-
-    public String cardsAsString() {
-        return cards().stream()
-                      .map(ConsoleCard::display)
-                      .collect(Collectors.joining(
-                            ansi().cursorUp(6).cursorRight(1).toString()));
+        System.out.println(ConsoleHand.cardsAsString(this));
     }
 
     // Query: snapshot in time,
