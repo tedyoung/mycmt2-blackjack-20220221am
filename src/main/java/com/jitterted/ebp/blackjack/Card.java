67bc369ffe6ec4939b6d4cb88a201dd5b01784f3
Ted M. Young <tedyoung@gmail.com>
2022-02-21 10:41:16 -0800

    LAB #1: Moved display() to new ConsoleCard class.

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -1,9 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import org.fusesource.jansi.Ansi;
-
-import static org.fusesource.jansi.Ansi.ansi;
-
 public class Card {
     private final Suit suit;
     private final Rank rank;
@@ -17,24 +13,6 @@ public class Card {
         return rank.value();
     }
 
-    public String display() {
-        String[] lines = new String[7];
-        lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", rank().display(), rank() == Rank.TEN ? "" : " ");
-        lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", suit().symbol());
-        lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", rank() == Rank.TEN ? "" : " ", rank().display());
-        lines[6] = "└─────────┘";
-
-        Ansi.Color cardColor = suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
-        return ansi()
-                .fg(cardColor).toString()
-                + String.join(ansi().cursorDown(1)
-                                    .cursorLeft(11)
-                                    .toString(), lines);
-    }
-
     public Suit suit() {
         return suit;
     }
@@ -43,6 +21,7 @@ public class Card {
         return rank;
     }
 
+    // for logging, troubleshooting, debugging, NOT DOMAIN
     @Override
     public String toString() {
         return "Card {" +
--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -10,7 +10,7 @@ class CardDisplayTest {
     public void displayTenAsString() throws Exception {
         Card card = new Card(Suit.CLUBS, Rank.TEN);
 
-        assertThat(card.display())
+        assertThat(ConsoleCard.display(card))
                 .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
     }
 
@@ -18,7 +18,7 @@ class CardDisplayTest {
     public void displayNonTenCardAsString() throws Exception {
         Card card = new Card(Suit.DIAMONDS, Rank.NINE);
 
-        assertThat(card.display())
+        assertThat(ConsoleCard.display(card))
                 .isEqualTo("[31m┌─────────┐[1B[11D│9        │[1B[11D│         │[1B[11D│    ♦    │[1B[11D│         │[1B[11D│        9│[1B[11D└─────────┘");
     }
 }
