295de3634e192ee52567c3013de84b4c4ac8950e
Ted M. Young <tedyoung@gmail.com>
2022-02-22 11:43:26 -0800

    LAB #4: Refactored test code to move test deck creation to StubDeck

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -8,10 +8,7 @@ class GameOutcomeTest {
 
     @Test
     public void playerHitsAndGoesBustThenPlayerLoses() throws Exception {
-        Deck playerHitsAndGoesBustDeck = new StubDeck(Rank.TEN,   Rank.EIGHT,
-                                                      Rank.QUEEN, Rank.JACK,
-                                                      Rank.THREE);
-        Game game = new Game(playerHitsAndGoesBustDeck);
+        Game game = new Game(StubDeck.playerHitsAndGoesBust());
         game.initialDeal();
 
         game.playerHits();
@@ -22,9 +19,7 @@ class GameOutcomeTest {
 
     @Test
     public void playerDealtBetterHandThanDealerAndStandsThenPlayerBeatsDealer() throws Exception {
-        Deck playerStandsAndBeatsDealer = new StubDeck(Rank.TEN, Rank.EIGHT,
-                                                       Rank.QUEEN, Rank.JACK);
-        Game game = new Game(playerStandsAndBeatsDealer);
+        Game game = new Game(StubDeck.playerStandsAndBeatsDealer());
         game.initialDeal();
 
         game.playerStands();
@@ -33,4 +28,5 @@ class GameOutcomeTest {
         assertThat(game.determineOutcome())
                 .isEqualTo("You beat the Dealer! 💵");
     }
+
 }
